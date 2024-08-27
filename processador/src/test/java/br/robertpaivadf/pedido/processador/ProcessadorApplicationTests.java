2f4b98d2c8f4e7342cf4215fa6bc9347031b0bbb
robertpaivadf <robertpaiva.df@gmail.com>
2024-08-27 16:42:57 -0300

    Add o módulo processador e configurando o pom.xml

---
+++ processador/src/test/java/br/robertpaivadf/pedido/processador/ProcessadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.robertpaivadf.pedido.processador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
